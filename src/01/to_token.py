f0780b04b4dbe23a01b6d43b2459b5adea675de0
AjxLab <abe12@mccc.jp>
2020-03-30 13:58:34 +0900

    チートシート

--- src/01/to_token.py
+++ src/01/to_token.py
@@ -7,3 +7,13 @@ text = input('input text : ')
 
 for token in t.tokenize(text):
     print(token)
+
+    '''
+    表層形：token.surface
+    品詞：token.part_of_speech
+    活用型：token.infl_type
+    活用形：token.infl_form
+    基本形：token.base_form
+    読み：token.reading
+    発音：token.phonetic
+    '''
